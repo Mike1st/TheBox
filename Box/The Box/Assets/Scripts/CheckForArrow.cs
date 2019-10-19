38ed0e9dab59a980183d30a9760eadfb07f1b221
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-10-19 13:32:47 -0400

    arrow
    Arrow is not detected by collider

--- Box/The Box/Assets/Scripts/CheckForArrow.cs
+++ Box/The Box/Assets/Scripts/CheckForArrow.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class CheckForArrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int score = 0;
+    Arrow arrow;
+    const int KEY_RIGHT = 90;
+    const int KEY_DOWN = 180;
+    const int KEY_UP = 0;
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        arrow = collision.gameObject.GetComponent<Arrow>();
+        int rotation = arrow.GetRotation();
+        CheckIfArrowCorrect(rotation);
+    }
+
+    private void CheckIfArrowCorrect(int rotation)
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (rotation == KEY_RIGHT)
+            {
+                score++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (rotation == KEY_DOWN)
+            {
+                score++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (rotation == KEY_UP)
+            {
+                score++;
+            }
+        }
+        Debug.Log(score);
     }
 }

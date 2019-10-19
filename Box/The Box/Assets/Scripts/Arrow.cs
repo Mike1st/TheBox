619fc8921edbf2926db35a25188fdcd1bf31f0bb
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-10-19 12:32:00 -0400

    arrows sorta working

--- Box/The Box/Assets/Scripts/Arrow.cs
+++ Box/The Box/Assets/Scripts/Arrow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     int rotation;
+
     Rigidbody2D rigidBody2D;
 
     private void Awake()
@@ -18,22 +19,35 @@ public class Arrow : MonoBehaviour
     public void ChangeArrowDirection()
     {
         int random = Random.Range(0, 3);
+        //---Arrow Faces Right
         if (random == 0)
         {
             rotation = 90;
             Debug.Log("-90");
         }
+        //---Arrow Faces Down
         if (random == 1)
         {
             rotation = 180;
             Debug.Log("180");
         }
+        //---Arrow Faces Up
         if (random == 2)
         {
             rotation = 0;
-            Debug.Log("270");
+            Debug.Log("0");
         }
 
         rigidBody2D.rotation = rotation;
+    }
+
+    public int GetRotation()
+    {
+        return rotation;
+    }
+
+    public void OnCollisionEnter2D()
+    {
+        Destroy(gameObject);
     }
 }

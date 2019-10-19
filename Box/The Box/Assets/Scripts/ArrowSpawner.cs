675de9a1208aad3ee67710b70f0d41c768e7f21a
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-10-19 09:57:51 -0400

    arrowSprite

---
+++ Box/The Box/Assets/Scripts/ArrowSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject arrow;
+    private new Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void temporaryForce()
+    {
+
+    }
+
+    IEnumerator 
+}

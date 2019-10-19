267d700f0439c6b03bf914efff363a15eead0724
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2019-10-19 16:37:17 -0400

    master

---
+++ Box/The Box/Assets/Scripts/CatBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBehavior : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite[] catSprites;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            int random = Random.Range(0, 2);
+            Debug.Log(random);
+            yield return new WaitForSeconds(5);
+            spriteRenderer.sprite = catSprites[random];
+        }
+
+    }
+}
